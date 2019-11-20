f1a1e5de48304f621315a184de6c2ce9f5cd7442
juanjlunar <juanjlunarc@gmail.com>
2019-11-20 09:11:55 -0400

    Initial configurations

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import { configureStore } from "./redux/store";
+import { Switch, Redirect, Route } from "react-router";
+import Home from './components/Home';
+import Header from "./components/Header";
+import Xhr from "./utils/Xhr";
+// import { apiLocal, apiProduction, appEnv } from "./configs/global";
+// action
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+// store
+const store = configureStore();
+// export const isIos = /iP(hone|od|ad)/i.test(window.navigator.platform);
+Xhr.baseUrl = 'http://localhost:5555/'
+const App = () => {
+    return (
+        <Provider store={store}>
+            <Router>
+                <Header />
+                    <Switch>
+                        <Route path="/" component={Home} exact />
+                    </Switch>
+            </Router>
+        </Provider>
+    );
+};
 
 export default App;
+
