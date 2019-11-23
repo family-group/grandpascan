95397d5ac55c7d899594a2e2159ee99e61a19292
juanjlunar <juanjlunarc@gmail.com>
2019-11-22 23:01:13 -0400

    Adding routes and constants in App

--- src/App.js
+++ src/App.js
@@ -3,24 +3,39 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import { configureStore } from "./redux/store";
 import { Switch, Redirect, Route } from "react-router";
-import Home from './components/Home';
+import HomeView from './components/HomeView';
+import TransactionsViewContainer from './containers/TransactionsViewContainer';
+import BlockViewContainer from './containers/BlockDetailsViewContainer';
+import TransactionDetailsViewContainer from './containers/TransactionDetailsViewContainer';
 import Header from "./components/Header";
 import Xhr from "./utils/Xhr";
 // import { apiLocal, apiProduction, appEnv } from "./configs/global";
 // action
 
+// global constants
+export const COINS = {
+    grandpa: 1000000,
+    son: 1000,
+    grandson: 1
+};
 
 // store
 const store = configureStore();
 // export const isIos = /iP(hone|od|ad)/i.test(window.navigator.platform);
 Xhr.baseUrl = 'http://localhost:5555/'
+// Xhr.baseUrl = 'http://192.168.1.146:5555/'
 const App = () => {
     return (
         <Provider store={store}>
             <Router>
                 <Header />
                     <Switch>
-                        <Route path="/" component={Home} exact />
+                        <Route path="/" component={HomeView} exact />
+                        <Route path="/address/:address/transactions" component={HomeView} exact />
+                        <Route path="/block/:blockHash" component={BlockViewContainer} exact />
+                        <Route path="/transaction/:transactionDataHash" component={TransactionDetailsViewContainer} exact />
+                        <Route path="/transactions" component={TransactionsViewContainer} exact />
+                        <Route path="/blocks" component={TransactionsViewContainer} exact />
                     </Switch>
             </Router>
         </Provider>
