156c5148cf82e77f930a58bdffa98bae1a50b2f3
juanjlunar <juanjlunarc@gmail.com>
2019-11-22 23:04:11 -0400

    HomeView added

---
+++ src/components/HomeView.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+// importing components
+import LatestOperationsContainer from '../containers/LatestOperationsContainer';
+// importing styles
+import './styles/HomeView.css';
+import AsideContainer from '../containers/AsideContainer';
+
+
+class HomeView extends React.Component {
+    render() {
+        return (
+            <main className="home-main flex-row">
+                <AsideContainer />
+                <div className="main-content">
+                    <LatestOperationsContainer
+                        type="BLOCK" 
+                        title="Latest Blocks"
+                        linkToText="View all"
+                        linkTo="blocks"
+                     />
+                    <LatestOperationsContainer
+                        type="TRANSACTION" 
+                        title="Latest Transactions"
+                        linkToText="View all"
+                        linkTo="transactions"
+                     />
+                </div>
+                
+            </main>
+        );
+    }
+}
+export default HomeView;
